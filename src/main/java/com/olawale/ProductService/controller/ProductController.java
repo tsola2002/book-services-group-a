95360de2b5476b0d0b942b726eeaf2714276e1dd
ola wale <90061003+Olawale223@users.noreply.github.com>
2025-05-28 15:57:05 +0100

    added cross origin

--- src/main/java/com/olawale/ProductService/controller/ProductController.java
+++ src/main/java/com/olawale/ProductService/controller/ProductController.java
@@ -5,16 +5,10 @@ import com.olawale.ProductService.entity.Product;
 import com.olawale.ProductService.service.ProductService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping({"api/v1/products"})
 public class ProductController {
     @Autowired
