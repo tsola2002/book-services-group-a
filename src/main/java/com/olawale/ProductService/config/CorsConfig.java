234a1a5cc8f92e15ee559256401adc7b32412895
ola wale <90061003+Olawale223@users.noreply.github.com>
2025-06-01 11:26:42 +0100

    cors maooing update

--- src/main/java/com/olawale/ProductService/config/CorsConfig.java
+++ src/main/java/com/olawale/ProductService/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/")
-                .allowedOrigins("http://localhost:3000") // Add more origins if needed
+                .allowedOrigins("http://localhost:5173") // Add more origins if needed
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
